c6685b7b383ff6fe9c9b83afc4fc6250e28bd54b
roman-lakhnov <lakhnov.roman@gmail.com>
2023-12-24 14:54:34 +0200

    css+

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Сторінка налаштувань функції MQTT</h2>
+      <form action="settings_MQTT.cgi" method="post" target="_blank">
+        <fieldset className="pick-variant">
+          <legend className="r">Налаштування:</legend>
+          <label htmlFor="Enable">Увімкнути функцію MQTT:</label>
+          <input type="radio" name="MQTT" value="on" defaultChecked className="bred" /> Так
+          <input type="radio" name="MQTT" value="off" className="bred" /> Ні<br /><br />
+          <label htmlFor="login" className="p">Логін:</label>
+          <input type="text" id="login" name="login" maxLength="30" className="q" /><br /><br />
+          <label htmlFor="pass" className="p">Пароль:</label>
+          <input type="password" id="pass" name="pass" maxLength="30" className="q" /><br /><br />
+          <label htmlFor="ip_addr" className="p">IP Адреса серверу:</label>
+          <input type="text" id="ip_addr" name="ip_addr" minLength="7" maxLength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" className="q" /><br /><br />
+          <label htmlFor="port" className="p">Порт серверу:</label>
+          <input type="number" id="port" name="port" min="1" max="65535" placeholder="за замовченням 1883" className="q" /><br /><br />
+          <input type="submit" value="Застосувати" className="button bgrn" />
+        </fieldset>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
